44a145d25972d24f27437884fd091a65506ef71d
Chris Rosenberry <chris@fifthstoryinteractive.com>
2018-12-16 15:53:56 -0500

    Punchlist updates, registration process, resources, and more

--- js/custom.js
+++ js/custom.js
@@ -248,3 +248,30 @@ $(function(){
     })
 })
 
+function parseURLParams(url) {
+	var queryStart = url.indexOf("?") + 1,
+		queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+		query = url.slice(queryStart, queryEnd - 1),
+		pairs = query.replace(/\+/g, " ").split("&"),
+		parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") return;
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=", 2);
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+
+		if (!parms.hasOwnProperty(n)) parms[n] = [];
+		parms[n].push(nv.length === 2 ? v : null);
+	}
+	return parms;
+}
+$( document ).ready(function() {
+var urlString = window.location.href;
+	urlParams = parseURLParams(urlString);
+	for (i in urlParams.tab) {
+		target = urlParams.tab[i];
+		document.getElementById(target).click();
+	}
+});
